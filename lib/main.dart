7fc5e554a0c2afbad835e8417685561d60aa14c7
FlutLab.io <>
2022-08-15 23:24:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Text(
-        'Bem vindo ao Primeiro Texto Flutter',
-        textDirection: TextDirection.ltr,
-      ),
-      Text(
-        'Bem vindo ao Primeiro Texto Flutter',
-        textDirection: TextDirection.ltr,
-      ),
-    );
+void main() => runApp(Column(
+      children: const <Widget>[
+        Text('Bem vindo ao Primeiro Texto Flutter'),
+        Text('Meu segundo texto!!!'),
+        Expanded(
+          child: FittedBox(
+            fit: BoxFit.contain, // otherwise the logo will be tiny
+            child: FlutterLogo(),
+          ),
+        ),
+      ],
+    ));
