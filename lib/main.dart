20ba99123b812328289e5d7079ae127db5d40914
FlutLab.io <>
2022-08-20 13:41:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: Text('teste'),
           appBar: AppBar(
             title: Text('Tranferência'),
           ),
           floatingActionButton: FloatingActionButton(
-              onPressed: () {}, child: const Icon(Icons.add)),
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
         ),
         debugShowCheckedModeBanner: false,
       ),
