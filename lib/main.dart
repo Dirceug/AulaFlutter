c318e103a60e7075feaff3d6186f17b35dc03198
FlutLab.io <>
2022-08-20 13:17:51 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Tranferência')
-          )
-        )
-        Text('Olá pessoal'),
+            title: Text('Tranferência'),
+          ),
+        ),
       ),
     );
