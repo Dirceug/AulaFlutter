8f97b19fe76f8e4fe01867b35f128a0076d0a942
FlutLab.io <>
2022-08-20 13:09:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: const <Widget>[
-        Text('Bem vindo ao Primeiro Texto Flutter',
-            textDirection: TextDirection.ltr),
-        Text('Meu segundo texto!!!', textDirection: TextDirection.ltr),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: FlutterLogo(),
-          ),
-        ),
-      ],
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: Text('Olá pessoal'),
+      ),
+    );
